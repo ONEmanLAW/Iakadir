867a840444fcf26d899a316aef14abbece1de190
ONEmanLAW <hugoculot.dev@gmail.com>
2025-12-29 15:04:46 +0100

    fixed an issue with keyboard

--- Iakadir/KeyboardObserver.swift
+++ Iakadir/KeyboardObserver.swift
@@ -8,20 +8,25 @@ import UIKit
 import Combine
 
 final class KeyboardObserver: ObservableObject {
-    @Published var isVisible: Bool = false
+    @Published private(set) var isVisible: Bool = false
 
     private var cancellables = Set<AnyCancellable>()
 
     init() {
-        NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
-            .sink { [weak self] _ in
-                self?.isVisible = true
-            }
-            .store(in: &cancellables)
+        let willShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .map { _ in true }
 
-        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
-            .sink { [weak self] _ in
-                self?.isVisible = false
+        let willHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in false }
+
+        willShow
+            .merge(with: willHide)
+            .removeDuplicates()
+            .receive(on: RunLoop.main)
+            .sink { [weak self] visible in
+                DispatchQueue.main.async {
+                    self?.isVisible = visible
+                }
             }
             .store(in: &cancellables)
     }
